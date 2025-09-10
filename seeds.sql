a0e965768eb1166a1fb6a584df905ae6f11db1b9
Sofinti <sofiairinasosaraszap@gmail.com>
2025-09-09 22:50:16 -0300

    Intento de MVC y acomodación de rutas. Arreglo en el docker-compose y agregación de seeds.sql

---
+++ seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO Usuarios (Correo, Contraseña) VALUES 
+('Juan Palmares', '12345678')
+('Lucía Fernández', 'luci1234'),
+('Mateo Rodríguez', 'mateo_pass99'),
+('Nicolás Gómez', 'nicoSecure!7');
